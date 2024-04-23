45e06a58004becc2bc7013a3138a876d372e7b82
Rohan Gupta <rohan.gupta@zopper.com>
2024-04-23 12:45:06 +0530

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world changed again and again and again!");
+import express from "express";
+import http from "http";
+import ServerConfig from "./config/serverConfig";
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(ServerConfig.PORT, () => {
+  console.log(`Server is up and running on Port ${ServerConfig.PORT}`);
+});
